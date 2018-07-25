768357cf808c42a88b0845882defa01e35c75bce
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-07-25 00:58:42 -0400

    added phone number and comment schemas

--- models.js
+++ models.js
@@ -1,30 +1,59 @@
 'use strict';
 
 const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
+//mongoose.Promise = global.Promise;
 
 const userProfileSchema = mongoose.Schema({
 	
-	fullName: {
-    	firstName: String,
-    	lastName: String
-  	},
-  	userName: {type: String, required: true}
+  	userName: {type: String, required: true},
+  	firstName: {type: String, required: true},
+  	lastName: {type: String, required: true},
+  	created: {type: Date, default: Date.now}
 });
 
-userProfileSchema.virtual('fullName').get(function() {
-  return `${this.fullName.firstName} ${this.fullName.lastName}`.trim();
+const userContributionSchema = mongoose.Schema({
+	
+  	userName: {type: String, required: true},
+  	firstName: {type: String, required: true},
+  	lastName: {type: String, required: true},
+  	created: {type: Date, default: Date.now}
 });
+
+const commentSchema = mongoose.Schema({
+    //Not sure if to include required or not - must include user id of person who created it
+    content: {type: String, required: true},
+    creator: {type: String, required: true},
+    created: {type: Date, default: Date.now}
+});
+
+const phoneNumberSchema = mongoose.Schema({
+  
+    //**** Should I include a requirement for number of digits?
+    phoneNumber: {type: Number, required: true},
+    flags: {type: Number, required: true},
+    comments: [commentSchema],
+    created: {type: Date, default: Date.now}
+});
+
+//*******Get is not a function?
+// userProfileSchema.virtual('fullName').get(function() {
+//   return `${this.fullName.firstName} ${this.fullName.lastName}`.trim();
+// });
 
 userProfileSchema.methods.serialize = function() {
   return {
     id: this._id,
-    fullName: this.fullName,
     userName: this.userName,
+    firstName: this.firstName,
+    lastName: this.lastName,
     created: this.created
   };
 };
 
 const UserProfile = mongoose.model('UserProfile', userProfileSchema);
+const PhoneNumber = mongoose.model('PhoneNumber', phoneNumberSchema);
+const Comment = mongoose.model('PhoneNumber', phoneNumberSchema);
 
-module.exports = {UserProfile};
+module.exports = {UserProfile,PhoneNumber};
+
+
