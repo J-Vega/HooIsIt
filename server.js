19de1e4a38c46cfcc2fee5df84eea1a4ce1378da
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-07-25 12:21:31 -0400

    added test data and comments to declaration and get request for list endpoint

--- server.js
+++ server.js
@@ -11,10 +11,33 @@ app.use(express.static('public'));
 app.use(express.json());
 
 const { DATABASE_URL, PORT } = require('./config');
-const { UserProfile } = require('./models');
+const {UserProfile,PhoneNumber,Comment, testdata, testcomment} = require('./models');
+//const { PhoneNumber } = require('./models');
 
 app.get("/signup", (req, res) => {
   res.sendFile(__dirname + "/public/signup.html");
+});
+
+//Just for testing...
+app.get("/list", (req, res) => {
+  testdata
+    .find()
+    // we're limiting because restaurants db has > 25,000
+    // documents, and that's too much to process/return
+    .limit(4)
+    // success callback: for each restaurant we got back, we'll
+    // call the `.serialize` instance method we've created in
+    // models.js in order to only expose the data we want the API return.    
+    .then(testdata => {
+      res.json({
+        testdata: testdata.map(
+          (testdata) => testdata.serialize())
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal server error' });
+    });
 });
 
 //Use later for testing
@@ -56,6 +79,10 @@ function closeServer() {
 //     console.info(`App listening on ${this.address().port}`);
 //   });
 // }
+
+if (require.main === module) {
+  runServer(DATABASE_URL).catch(err => console.error(err));
+}
 
 app.listen(process.env.PORT || 8080);
 
