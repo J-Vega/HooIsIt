5a8b064b3bdd5d589a2ff0e640f126217e0e4be9
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-07-25 20:44:15 -0400

    added mlab collection url

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 'use strict';
 
 exports.DATABASE_URL = 
-	process.env.DATABASE_URL || 'mongodb://localhost/TeleTale';
+	process.env.DATABASE_URL || 'mongodb://user:abc123@ds153851.mlab.com:53851/teletale';
 //exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog-app';
 exports.PORT = process.env.PORT || 8080;
