bffbc8280aa53115ecae6471739f642ae5d34cc5
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-07-28 11:33:28 -0400

    added test url export

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 
 exports.DATABASE_URL = 
 	process.env.DATABASE_URL || 'mongodb://user:abc123@ds153851.mlab.com:53851/teletale';
-//exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-blog-app';
+exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-teletale';
 exports.PORT = process.env.PORT || 8080;
