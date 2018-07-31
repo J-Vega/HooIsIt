f4c05b204e07fb78c81a416744d2e1c5e5ab70f2
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-07-30 21:00:45 -0400

    changed test url from local to mlab url

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ exports.DATABASE_URL =
 	process.env.DATABASE_URL || 'mongodb://user:abc123@ds153851.mlab.com:53851/teletale';
 
 exports.TEST_DATABASE_URL = 
-	process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-teletale';
+	process.env.TEST_DATABASE_URL || 'mongodb://user:abc123@ds161391.mlab.com:61391/test-teletale';
 
 exports.PORT = process.env.PORT || 8080;
