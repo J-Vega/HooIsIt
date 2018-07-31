a2f6d4114ef9d946fb1daed3a258bab0a24449fb
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-07-30 20:57:41 -0400

    added comment schema and new mlab db names to userprofile schema, commented out test schemas

--- models.js
+++ models.js
@@ -2,17 +2,16 @@
 
 const mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-//mongoose.Promise = global.Promise;
 
 const userProfileSchema = mongoose.Schema({
 	
-  	displayName: {type: String, required: true, unique: true},
+  	userName: {type: String, required: true, unique: true},
   	firstName: {type: String, required: true},
   	lastName: {type: String, required: true},
     email: {type: String, required: true, unique: true},
   	created: {type: Date, default: Date.now},
-    userComments: {type:Schema.Types.ObjectId, ref: 'userComment'}
-});
+    userComments: [{type:Schema.Types.ObjectId, ref: 'userComment'}]
+},{collection:"userdata"});
 
 const userCommentSchema = mongoose.Schema({
     
@@ -21,45 +20,38 @@ const userCommentSchema = mongoose.Schema({
     created: {type: Date, default: Date.now}
 });
 
-const commentSchema = mongoose.Schema({
-    //Not sure if to include required or not - must include user id of person who created it
-    content: {type: String, required: true},
-    creator: [{type: Schema.Types.ObjectId, ref: 'UserProfile'}],
-    created: {type: Date, required: true, default: Date.now, }
-});
-
 const phoneNumberSchema = mongoose.Schema({
     
     phoneNumber: {type: Number, required: true},
     flags: {type: Number, required: true},
     description: {type:String, required: true},
-    comments: [{type:String}],
+    comments: [{type:String}],//[userCommentSchema],
     created: {type: Date, default: Date.now}
 
 },{collection:"phoneNumberData"});
 
-const testCommentSchema = mongoose.Schema({
-    //Not sure if to include required or not - must include user id of person who created it
-    //content: {type:PhoneNumber },
-    created: {type: Date, default: Date.now}
-});
+// const testCommentSchema = mongoose.Schema({
+//     //Not sure if to include required or not - must include user id of person who created it
+//     //content: {type:PhoneNumber },
+//     created: {type: Date, default: Date.now}
+// });
 
-const testdataSchema = mongoose.Schema({
-  phoneNumber: {type: Number, required: true, unique: true},
-  flags: {type: Number, required: true},
-  description: {type: String, required: true},
-  comments: [{type:String}],
-  created: {type: Date, default: Date.now}
-},{collection:"testdata"});
+// const testdataSchema = mongoose.Schema({
+//   phoneNumber: {type: Number, required: true, unique: true},
+//   flags: {type: Number, required: true},
+//   description: {type: String, required: true},
+//   comments: [{type:String}],
+//   created: {type: Date, default: Date.now}
+// },{collection:"testdata"});
 
 // userProfileSchema.virtual('virtualDisplayName').get(function() {
 //   return `${this.fullName.firstName} ${this.fullName.lastName}`.trim();
 // });
 
-commentSchema.pre('findOne', function(next){
-  this.populate(displayName);
-  next();
-});
+// commentSchema.pre('findOne', function(next){
+//   this.populate(displayName);
+//   next();
+// });
 
 userProfileSchema.methods.serialize = function(){
   return {
@@ -67,11 +59,22 @@ userProfileSchema.methods.serialize = function(){
     userName: this.userName,
     firstName: this.firstName,
     lastName: this.lastName,
+    email: this.email,
+    userComments: this.userComments,
+    created: this.created
+  };
+};
+phoneNumberSchema.methods.serialize = function(){
+  return{
+    id: this._id,
+    phoneNumber: this.phoneNumber,
+    description: this.description,
+    comments: this.comments,
     created: this.created
   };
 };
 
-commentSchema.methods.serialize = function(){
+userCommentSchema.methods.serialize = function(){
   return{
     id: this._id,
     content: this.content,
@@ -83,10 +86,10 @@ commentSchema.methods.serialize = function(){
 const UserProfile = mongoose.model('UserProfile', userProfileSchema);
 const PhoneNumber = mongoose.model('PhoneNumber', phoneNumberSchema);
 const UserComment = mongoose.model('UserComment', userCommentSchema)
-const Comment = mongoose.model('Comment', commentSchema);
-const testdata = mongoose.model('testdata', testdataSchema);
-const testcomment = mongoose.model('testcomment', testCommentSchema);
+//const Comment = mongoose.model('Comment', commentSchema);
+// const testdata = mongoose.model('testdata', testdataSchema);
+// const testcomment = mongoose.model('testcomment', testCommentSchema);
 
-module.exports = {UserProfile,PhoneNumber,Comment, testdata, testcomment};
+module.exports = {UserProfile,PhoneNumber,UserComment};// testdata, testcomment};
 
 
