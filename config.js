8cc1f74a9c1201f9c8a018cd6ab1982d04ad7ab1
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-08-23 12:34:45 -0400

    Added jwt exports

--- config.js
+++ config.js
@@ -6,4 +6,9 @@ exports.DATABASE_URL =
 exports.TEST_DATABASE_URL = 
 	process.env.TEST_DATABASE_URL || 'mongodb://user:abc123@ds161391.mlab.com:61391/test-teletale';
 
+exports.JWT_SECRET = 'a'; //Key used to create encrypted pw
+
+exports.JWT_EXPIRY = '7d';
+//Google pw salt - gives random 64 bit character string
+
 exports.PORT = process.env.PORT || 8080;
