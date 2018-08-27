7214400891c354ca15649877d05992fd97450b42
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-08-27 17:37:58 -0400

    added email to fake data seed

--- test/test-server.js
+++ test/test-server.js
@@ -59,13 +59,13 @@ function generateUserData(){
 	var firstName = faker.name.firstName();
 	var lastName = faker.name.lastName();
   var password = "abcd123456";
-	//var email = faker.random.word()+"@"+faker.random.word()+".com";
+	var email = faker.random.word()+"@"+faker.random.word()+".com";
 
 	return{
 		userName: userName,
     	firstName: firstName,
     	lastName: lastName,
-    	//email: email,
+    	email: email,
       password: password,
     	comments: [],
     	created: faker.date.past()
@@ -261,12 +261,13 @@ describe('User data API resource', function(){
           		expect(res).to.be.json;
           		expect(res.body).to.be.a('object');
           		expect(res.body).to.include.keys(
-            		'id','userName', 'firstName', 'lastName');//, 'email');
+            		'id','userName', 'firstName', 'lastName', 'email');
           			//Serialized model - id -vs actual model - _id -
          		expect(res.body.id).to.not.be.null;
           		expect(res.body.userName).to.equal(newUser.userName);
           		expect(res.body.firstName).to.equal(newUser.firstName);
           		expect(res.body.lastName).to.equal(newUser.lastName);
+              expect(res.body.email).to.equal(newUser.email);
               expect(res.body.password).to.not.equal(null);
           		//expect(res.body.email).to.equal(newUser.email);
           	return UserProfile.findById(res.body.id);
@@ -276,6 +277,7 @@ describe('User data API resource', function(){
           	expect(listing.userName).to.equal(newUser.userName);
           	expect(listing.firstName).to.equal(newUser.firstName);
           	expect(listing.lastName).to.equal(newUser.lastName);
+            expect(listing.email).to.equal(newUser.email);
             expect(listing.password).to.not.equal(null);
           	//expect(listing.email).to.equal(newUser.email);
         });
