7c2882afce56886d08767dbb36f1dfc52da24002
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-09-03 16:02:48 -0400

    redeploying to test on heroku

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.DATABASE_URL = 
-	process.env.DATABASE_URL || 'mongodb://user:abc123@ds153851.mlab.com:53851/teletale';
+	process.env.DATABASE_URL || 'mongodb://user:abc123@ds153851.mlab.com:53851/teletale'; 
 
 exports.TEST_DATABASE_URL = 
 	process.env.TEST_DATABASE_URL || 'mongodb://user:abc123@ds161391.mlab.com:61391/test-teletale';
