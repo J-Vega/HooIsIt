993f19db376a0260af4997894d65f7cd5740fbab
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-09-04 12:36:37 -0400

    Removed unnecessary comments and tweaked spacing

--- public/listing.js
+++ public/listing.js
@@ -1,14 +1,4 @@
 
-//This script is only called from listing.html
-//	and is used to do a GET request of whatever phone number is being searched / listed
-//js-phone-number
-
-//html5 
-//.com/search/23423423235
-
-//.com/search/?var=334445555
-$('.no-data').hide();
-$('.has-data').hide();
 
 let phoneNumber = window.location.search.slice(1); 
 let phoneId = "";
@@ -17,29 +7,21 @@ function watchSubmit(){
 	$('.js-listing-search').submit(event => 
 	{ 
 		event.preventDefault();
-
 		const numberQuery = $(event.currentTarget).find('.js-navSearch').val();
-		
-		
 		window.location.href = `listing.html?${numberQuery}`;
-
 	});	
 }
 
 function getNumberData(number, callback){
 	
-	//let numberFromUrl = number.slice(1);
-	//console.log(numberFromUrl);
 	document.getElementById("js-phone-number-1").innerHTML = number;
 	console.log(`Searching for phone number ${number}`);
 	
 	let query = {
-		//url: "https://stormy-tundra-36765.herokuapp.com/list",
 		url: `/search/${number}`,
 		dataType: 'json'
-		//success: callback
 	}
-	console.log("getting json data from - " + query.url);
+
 	$.getJSON(query,callback);
 }
 
@@ -52,12 +34,10 @@ function displaySearchData(data){
 		$('.js-results').html("<p>There are currently no comments.</p>");
 	}
 	else{
-		//***********    Concern -- DID NOT use data.map since the listing is an object and not an array
-		//const results = data.phoneNumber.map((item,index) => renderResults(item,index));
-		//console.log(`${results}`);
 		console.log("There's data!");
 		$('.has-data').show();
-		
+		$('.no-data').hide();
+		$('.add-comment-form').hide();
 		const results = renderListing(data);
 		$('.js-results').html(results);
 
@@ -71,27 +51,22 @@ function renderListing(result){
 	console.log(result);
 	// Reverses comments, so most recent is rendered first.
 	result["comments"].reverse();
-	let commentList = "";//result.comments[0].content;
+	let commentList = "";
 	for(i = 0; i < result["comments"].length; i++){
-		// console.log(result.comments[i].content);
+		
 		commentList += `
 		<div class = "commentBlock"><p class ="comment">'${result.comments[i].content}'</p>
 		<p class ="commenter">Posted by: ${result.comments[i].creator} 
 			on ${result.comments[i].created.slice(0,10)}</p></div>`;
 	}
-	// console.log(commentList);
-	//console.log(index);
+
 	savePostId(result["_id"]);
-	console.log(result["_id"]);
 	return `	
-				<h3>Previous reports for - ${result["phoneNumber"]}</h3>
-				<h3>Type of call: ${result["description"]}</h3>
-				
-				<h4>Comments (${result["comments"].length}): </h4><p>${commentList}</p>
-			`
-			// Most likely going to get rid of 'flags'
-			// <h3>This number has been flagged ${result["flags"]} time(s). </h3>
-	}
+		<h3>Previous reports for - ${result["phoneNumber"]}</h3>
+		<h3>Type of call: ${result["description"]}</h3>
+		
+		<h4>Comments (${result["comments"].length}): </h4><p>${commentList}</p>`;
+}
 
 function savePostId(id){
 	console.log(id);
@@ -125,7 +100,6 @@ function addComment(author,comment){
     	success: function( data, textStatus, jQxhr ){
         console.log("Success!");
         window.location.reload();
-        //$('#response pre').html( JSON.stringify( data ) );
     	},
     	error: function( jqXhr, textStatus, errorThrown ){
         console.log( errorThrown );
